81d71936618ae1539d2d69cce8b0e0d0121b29bf
Rampage-ZA <imraan.kajee@gmail.com>
2019-05-17 20:39:40 +0200

    Add files via upload

--- testnet.ps1
+++ testnet.ps1
@@ -3,10 +3,9 @@ do
 While (!(Test-Connection -computer google.com -count 1 -quiet)) 
 	{
 	Disable-NetAdapter -Name Wi-Fi -Confirm:$false
-	Start-Sleep -Seconds 1
 	Enable-NetAdapter -Name Wi-Fi -Confirm:$false
 	Start-Sleep -Seconds 15
 	}
 }
 
-until(Start-Sleep -Seconds 15)
+until(Start-Sleep -Seconds 5)
